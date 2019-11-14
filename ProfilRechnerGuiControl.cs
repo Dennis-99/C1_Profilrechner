61f5e0aa2d9e307a9ef6332fff2e50cd50a3b5af
Colin2123 <56160072+Colin2123@users.noreply.github.com>
2019-11-14 15:37:08 +0100

    Add files via upload

---
+++ ProfilRechnerGuiControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Gedöns
+{
+    public class ProfilRechnerGuiControll
+    {
+        //Aufrauf des Gui Fensters
+        ProfilRechnerGuiControll()
+        {
+            Window fenster1 = new Window();
+            fenster1.Title = "ProfilRechner";
+            fenster1.SizeToContent = SizeToContent.WidthAndHeight;
+            fenster1.ResizeMode = ResizeMode.NoResize;
+            UserControl1 PRGui1 = new UserControl1();
+
+            fenster1.Content = PRGui1;
+            fenster1.ShowDialog();
+
+            Console.WriteLine("Taste Drücken zum Beenden");
+            Console.ReadKey();
+
+
+
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new ProfilRechnerGuiControll();
+            
+        }
+    }
+}
