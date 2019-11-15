44782d012ed349852c8d11b7f14116eae11449fe
Lukas-21 <56160298+Lukas-21@users.noreply.github.com>
2019-11-15 19:29:12 +0100

    Add files via upload

---
+++ GraphicLayout/GraphicLayout/UserControl1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphicLayout
+{
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SpracheD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_SpracheE_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_SpracheF_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
